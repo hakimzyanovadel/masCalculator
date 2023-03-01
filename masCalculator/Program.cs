28548f101554da997790eaa8387b17cb32c44197
Адель Хакимзянов <adelhakimzanov68@gmail.com>
2023-03-01 20:19:40 +0300

    Добавьте файлы проекта.

---
+++ masCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace masCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Введите начальное значение диапазона");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное значение диапазона");
+            b = int.Parse(Console.ReadLine());
+
+            int height;
+            Console.WriteLine("Введите количество строк в массиве");
+            height = int.Parse(Console.ReadLine());
+            int width;
+            Console.WriteLine("Введите количество столбцов в массиве");
+            width = int.Parse(Console.ReadLine());
+
+            int[,] mas = new int[height, width];
+
+            Random r = new Random();
+            int k = 0;
+           
+            for (int i = 0; i < height; i++)
+            {
+                int l = 0;
+                for ( int j = 0; j < width; j++) 
+                {
+                    mas[k, l] = r.Next(a, b);
+                    Console.Write(mas[k, l] + " ");
+                    l++;
+                }
+                Console.WriteLine();
+                k++;
+            }
+        }
+    }
+}
